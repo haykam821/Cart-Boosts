c46092e8967e955ecd0d55ccc62d131ae3249b29
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-04-15 15:25:50 -0400

    Implement basic functionality

---
+++ src/main/java/io/github/haykam821/cartboosts/mixin/AbstractMinecartEntityMixin.java
@@ -0,0 +1,15 @@
+package io.github.haykam821.cartboosts.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+import net.minecraft.entity.vehicle.AbstractMinecartEntity;
+
+@Mixin(AbstractMinecartEntity.class)
+public class AbstractMinecartEntityMixin {
+	@ModifyConstant(method = "pushAwayFrom", constant = @Constant(doubleValue = 0.800000011920929d))
+	private double makeMinecartsPushEachOther(double value) {
+		return Double.MIN_VALUE;
+	}
+}
